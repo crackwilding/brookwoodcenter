9f23691eb51837906ba08270602cbb5b79ed7856
Fletch <fletch@gatech.edu>
2024-11-12 16:55:39 -0500

    vendor files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit9240392502bc9cb6b8bf362457f91124
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit9240392502bc9cb6b8bf362457f91124', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit9240392502bc9cb6b8bf362457f91124', 'loadClassLoader'));
